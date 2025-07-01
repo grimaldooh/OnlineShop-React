48a91f96187ec648694da95e0d5fb05d990e9b38
grimaldooh <angel.grimaldo@uabc.edu.mx>
2025-06-30 21:58:51 -0700

    fix theme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -68,7 +68,7 @@ export default function Navbar() {
   }
 
   return (
-    <nav className="flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light bg-white">
+    <nav className="flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light bg-dark">
       <ul className="flex items-center gap-3">
         <li className="font-semibold text-lg flex items-center">
           
